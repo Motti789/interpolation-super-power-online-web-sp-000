3b98cfe23b0a240736c5232ebf1e4529aa083daf
Motti Wilschanski <mchillywilly@gmail.com>
2021-04-25 22:52:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 def display_rainbow(colors)
 puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+=======
+def #display_rainbow(colors)
+puts "R: red, #{colors[0]}, O: orange, #{colors[1]}, Y: yellow, #{colors[2]}"
+puts "G: green, #{colors[3]}, B: blue #{colors[4]}, I: indigo,#{colors[5]},"
+puts "V: violet, #{colors[6]}"
+>>>>>>> 1ffbe73dfd36d9899f3c4956a1f466f3edfc8bca
 end
